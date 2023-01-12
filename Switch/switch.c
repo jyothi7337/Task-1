a172ac6beafd43f3697b26674122cb6db8c1ed99
jyothi7337 <boyapally.jyothirmai@capgemini.com>
2023-01-12 16:33:44 +0530

    little modified

--- Switch/switch.c
+++ Switch/switch.c
@@ -12,8 +12,6 @@
 #include <linux/err.h>
 #include <linux/jiffies.h>
 
-extern unsigned long volatile jiffies;
-unsigned long old_jiffie = 0;
 
 
 #define GPIO_21_OUT (21)
@@ -27,23 +25,20 @@ unsigned int led_toggle = 0;
 
 unsigned int gpio_irq_number;
 
-int delay_flag = 0;
+
 
 static irqreturn_t gpio_irq_handler(int irq,void *dev_id) {
-	static unsigned long flags = 0;
-	unsigned long diff = jiffies - old_jiffie;
-	if (diff < 20){
-		delay_flag = 1;
-		return IRQ_HANDLED;
-	}
-	old_jiffie = jiffies;  
-
-	local_irq_save(flags);
-	led_toggle = (1 ^ led_toggle);                             
-	gpio_set_value(GPIO_21_OUT, led_toggle);                      
+	uint8_t switch_state = 0;
+	switch_state = gpio_get_value(GPIO_20_IN);
+	gpio_set_value(GPIO_21_OUT,switch_state);                      
 	printk("Interrupt Occurred : GPIO_21_OUT : %d ",gpio_get_value(GPIO_21_OUT));
-	local_irq_restore(flags);
-	delay_flag = 0;
+	if(switch_state == 1){
+		mdelay(1000);
+	}
+	else if(switch_state == 0){
+		mdelay(500);
+	}
+
 	return IRQ_HANDLED;
 }
 
@@ -56,12 +51,10 @@ static int switch_open(struct inode *inode, struct file *file){
 	printk("Device File Opened...!!!\n");
 	if(gpio_is_valid(GPIO_20_IN) == false){
 		printk("GPIO - %d is not valid\n",GPIO_20_IN);
-		gpio_free(GPIO_20_IN);
 		return -1;
 	}
 	if(gpio_request(GPIO_20_IN,"GPIO_20_IN") < 0){
 		printk("GPIO -%d faild to request\n",GPIO_20_IN);
-		gpio_free(GPIO_20_IN);
 		return -1;
 	}
 	if(gpio_direction_input(GPIO_20_IN) <0){
@@ -71,9 +64,8 @@ static int switch_open(struct inode *inode, struct file *file){
 	}
 	gpio_irq_number = gpio_to_irq(GPIO_20_IN);
 	printk("GPIO IRQ Number = %d\n", gpio_irq_number);
-	if (request_irq(gpio_irq_number,(void *)gpio_irq_handler,IRQF_TRIGGER_RISING,"switch_device",NULL)){                    
+	if (request_irq(gpio_irq_number,(void *)gpio_irq_handler,IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,"switch_device",NULL)){                    
 		printk("Switch cannot register IRQ\n");
-		free_irq(gpio_irq_number,NULL);
 		return -1;
 	}
 	return 0;
@@ -81,8 +73,8 @@ static int switch_open(struct inode *inode, struct file *file){
 
 static int switch_release(struct inode *inode, struct file *file){
 	printk("Device File Closed...!!!\n");
-	gpio_free(GPIO_20_IN);
 	free_irq(gpio_irq_number,NULL);
+	gpio_free(GPIO_20_IN);
 	return 0;
 }
 
@@ -91,14 +83,8 @@ static ssize_t switch_read(struct file *filp,char __user *buf, size_t len, loff_
 	uint8_t gpio_state = 0;
 
 	gpio_state = gpio_get_value(GPIO_20_IN);
-
-	len = 1;
-	if(delay_flag == 1){
-		printk("Waiting for debounce\n");
-		mdelay(200);
-	}
-	if( copy_to_user(buf, &gpio_state, len) > 0) {
-		pr_err("ERROR: Not all the bytes have been copied to user\n");
+	if (len<1){
+		printk("buffer size is invalid\n");
 		return -1;
 	}
 
@@ -120,7 +106,6 @@ static int __init switch_driver_init(void){
 	int err = alloc_chrdev_region(&switch_dev, 0, 1, "switch_Dev");
 	if( err < 0){
 		printk("Failed to allocate character device region\n");
-		unregister_chrdev_region(switch_dev, 1);
 		return -1;
 	}
 	printk("Major = %d Minor = %d \n",MAJOR(switch_dev), MINOR(switch_dev));
@@ -130,7 +115,6 @@ static int __init switch_driver_init(void){
 
 	if(cdev_add(&switch_cdev,switch_dev,1) < 0){
 		printk("Failed at cdev_add\n");
-		cdev_del(&switch_cdev);
 		unregister_chrdev_region(switch_dev, 1);
 		return -1;
 	}
@@ -138,7 +122,6 @@ static int __init switch_driver_init(void){
 
 	if(IS_ERR(switch_class)){
 		printk("Failed to create class\n");
-		class_destroy(switch_class);
 		cdev_del(&switch_cdev);
 		unregister_chrdev_region(switch_dev, 1);
 		return -1;
@@ -149,7 +132,6 @@ static int __init switch_driver_init(void){
 		class_destroy(switch_class);
 		cdev_del(&switch_cdev);
 		unregister_chrdev_region(switch_dev, 1);
-		device_destroy(switch_class,switch_dev);
 		return -1;
 	}
 
